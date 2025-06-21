6dd25204238c708b977a179e2f4a837872e2fb50
jagadesh.alpha8 <jagadesh.alpha8@gmail.com>
2025-06-21 12:31:14 +0530

    false and ip

--- skill/skill/settings.py
+++ skill/skill/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^#$5*aqtf-i0cv!dygkw9*hdhk97km_vgyi@yu$r(ztefkzta7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
