47080c74593d9be0b0748e037789d3683ef4349a
jagadesh.alpha8 <jagadesh.alpha8@gmail.com>
2025-06-21 17:29:17 +0530

    caps

--- skill/skill/settings.py
+++ skill/skill/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^#$5*aqtf-i0cv!dygkw9*hdhk97km_vgyi@yu$r(ztefkzta7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
